66dfd0eacecd25cf0baa970a480a080d7a42c6dd
Rodrigo Chacon <rochacon@gmail.com>
2017-12-14 14:30:51 +0100

    chore(setup.py) bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='jiraya',
     description='Simple Jira Command Line Interface.',
-    version='0.1.1',
+    version='0.1.2',
     install_requires=open('requirements.txt').readlines(),
     scripts=['jiraya'],
 )
