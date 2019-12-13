58a102b4fabb23c38c678a12e1339c00581a2848
Rodrigo Chacon <rochacon@gmail.com>
2019-12-13 11:14:07 -0300

    chore(setup.py) fix long description
    Signed-off-by: Rodrigo Chacon <rochacon@gmail.com>

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 setup(
     name='jiraya',
     description='Simple Jira Command Line Interface.',
+    long_description='Simple Jira Command Line Interface.',
     author='Rodrigo Chacon',
     author_email='rochacon@gmail.com',
     url='https://github.com/rochacon/jiraya',
