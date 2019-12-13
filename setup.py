ae59822d80ff0ae57b085825b9ae087d5ac0b350
Rodrigo Chacon <rochacon@gmail.com>
2019-12-13 11:04:45 -0300

    chore(setup.py) PyPI release
    Signed-off-by: Rodrigo Chacon <rochacon@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ from setuptools import setup
 setup(
     name='jiraya',
     description='Simple Jira Command Line Interface.',
-    version='0.1.2',
+    author='Rodrigo Chacon',
+    author_email='rochacon@gmail.com',
+    url='https://github.com/rochacon/jiraya',
+    license='MIT',
+    version='0.1.3',
     install_requires=open('requirements.txt').readlines(),
     scripts=['jiraya'],
 )
